3b74d134bf4b0b3254bbce6ca4029a3b657dd308
unknown <s11810068@student.unklab.ac.id>
2021-05-05 23:01:51 +0800

    second commit

---
+++ src/components/molecules/NavbarFooter/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavbarFooter = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand text-light">Project Final Back-End Web Development</a>
+      </div>
+    </nav>
+  );
+};
+
+export default NavbarFooter;
---
+++ src/components/molecules/NavbarHeader/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavbarHeader = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-warning">
+      <div className="container-fluid">
+        <a className="navbar-brand text-dark"><b>QuickNotes</b></a>
+      </div>
+    </nav>
+  );
+};
+
+export default NavbarHeader;
